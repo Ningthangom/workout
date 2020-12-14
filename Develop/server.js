a2e9c6e6f61827b9f854b34116de275be035c5b0
Ningthangom <ningthangom@gmail.com>
2020-12-14 21:37:41 +1030

    added string of connection from mongo atlas

--- Develop/server.js
+++ Develop/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://Ningthanom:Ningthangom123@cluster0.dm7f2.mongodb.net/workout_db?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
